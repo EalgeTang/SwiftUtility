56f4d6fc64bb889b3f4ba6ba5203fe3efb4cfe3f
唐博文 <tangbowen@ireadercity.com>
2020-05-21 18:20:56 +0800

    ...

--- SwiftUtility/SwiftUtility/ViewController.swift
+++ SwiftUtility/SwiftUtility/ViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
-
+typealias block = ()-> Bool
 class ViewController: UIViewController {
 
+    lazy var arr: [String] =  {
+        let array = ["1", "2"]
+        return array
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = tkRandowColor()
-        self.view.tt.addTapGesture(self, action: #selector(abcd))
+        view.tt.addTapGesture(self, action: #selector(abcd))
+        view.backgroundColor = .red
+        let aa = makeIncrementer(forIncrement: 10)
+        let value1 = aa()
+        print("value is \(value1)")
+        let value2 = aa()
+        print("value is \(value2)")
+        let value3 = aa()
+        print("value is \(value3)")
+        let value4 = aa()
+        print("value is \(value4)")
+        
+        DLog("array is \(arr)")
+        
     }
     
-    @objc func abcd(){
+    func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+        var runningTotal = 0
+        func incrementer() -> Int{
+            runningTotal += amount
+            return runningTotal
+        }
+        return incrementer
+    }
+    @objc func abcd(block:block){
             
     }
 }
